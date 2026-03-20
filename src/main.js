da76eb6a171fdcd09a20364b0d6a3776ba9d3c73
haoweilu <haowei.lu@auo.com>
2026-03-20 10:59:36 +0800

    feat: add Cyberpunk global styles and page transitions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import './styles/global.css'
+import './styles/transitions.css'
+import './styles/glitch.css'
 
 const app = createApp(App)
+app.use(createPinia())
 app.mount('#app')
